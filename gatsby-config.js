7a6ea9102ba4086ed4fcfbfa2f4d84b217250031
Paul Bennett-Freeman <clicknmix@gmail.com>
2020-05-13 22:58:11 +0100

    🔌 Configure strapi with ENV config and to pull short-urls

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,17 +1,26 @@
 module.exports = {
   siteMetadata: {
-    title: `A Site By Paul Bennett-Freeman`,
+    title: `A Site By Paul Bennett-Freeman`
   },
   plugins: [
-    {
-      resolve: `gatsby-source-datocms`,
-      options: {
-        apiToken: process.env.DATO_API_TOKEN,
-      },
-    },
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-typescript`,
     "gatsby-transformer-typescript-css-modules",
-    'gatsby-plugin-postcss',
-  ],
-}
+    "gatsby-plugin-postcss",
+    {
+      resolve: `gatsby-source-strapi`,
+      options: {
+        apiURL: process.env.STRAPI_URL,
+        queryLimit: 1000, // Default to 100
+        contentTypes: [`portfolio`, "short-urls"],
+        //If using single types place them in this array.
+        singleTypes: [],
+        // Possibility to login with a strapi user, when content types are not publically available (optional).
+        loginData: {
+          identifier: process.env.STRAPI_USER,
+          password: process.env.STRAPI_PASSWORD
+        }
+      }
+    }
+  ]
+};
