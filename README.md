3fd1adbd79515be7e56ff8caddebf613622cd8fa
Paul Bennett-Freeman <clicknmix@gmail.com>
2020-06-08 18:30:18 +0100

    📚 Add new contentTypes to Strapi sources

--- README.md
+++ README.md
@@ -1,16 +1,28 @@
 # gatsby-theme-oddessay
 
-The theme that drives my websites. 
+The theme that drives my websites.
 
 Might be a useful reference for someone.
 
 ## ENV
 
-`DATO_API_TOKEN` needs to be set with your Read Only Token which can be found in "Settings" -> "API Tokens"
+`STRAPI_USER` needs to be set to the Strapi username.
+`STRAPI_PASSWORD` needs to be set to the Strapi user's password.
+`STRAPI_URL` is the url of the Strapi server.
+
+# Adding Strapi Content Types
+
+`gatsby-source-strapi.options.contentTypes` should be updated with any new content types as they are added.
 
 ## Commands
 
 | Command          |                          |
-|------------------|--------------------------|
+| ---------------- | ------------------------ |
 | `yarn storybook` | Runs and opens Storybook |
 | `yarn develop`   | Runs Gatsby              |
+
+# Building a new Gatsby Site with this theme
+
+`gatsby new <name>`
+`yarn install`
+`yarn add https://github.com/OddEssay/gatsby-theme-oddessay`
--- gatsby-config.js
+++ gatsby-config.js
@@ -1,26 +1,26 @@
 module.exports = {
-  siteMetadata: {
-    title: `A Site By Paul Bennett-Freeman`
-  },
-  plugins: [
-    `gatsby-plugin-react-helmet`,
-    `gatsby-plugin-typescript`,
-    "gatsby-transformer-typescript-css-modules",
-    "gatsby-plugin-postcss",
-    {
-      resolve: `gatsby-source-strapi`,
-      options: {
-        apiURL: process.env.STRAPI_URL,
-        queryLimit: 1000, // Default to 100
-        contentTypes: [`portfolio`, "short-urls"],
-        //If using single types place them in this array.
-        singleTypes: [],
-        // Possibility to login with a strapi user, when content types are not publically available (optional).
-        loginData: {
-          identifier: process.env.STRAPI_USER,
-          password: process.env.STRAPI_PASSWORD
+    siteMetadata: {
+        title: `A Site By Paul Bennett-Freeman`
+    },
+    plugins: [
+        `gatsby-plugin-react-helmet`,
+        `gatsby-plugin-typescript`,
+        "gatsby-transformer-typescript-css-modules",
+        "gatsby-plugin-postcss",
+        {
+            resolve: `gatsby-source-strapi`,
+            options: {
+                apiURL: process.env.STRAPI_URL,
+                queryLimit: 1000, // Default to 100
+                contentTypes: [`portfolio`, `short-urls`, `blog-posts`, `sites`],
+                //If using single types place them in this array.
+                singleTypes: [],
+                // Possibility to login with a strapi user, when content types are not publically available (optional).
+                loginData: {
+                    identifier: process.env.STRAPI_USER,
+                    password: process.env.STRAPI_PASSWORD
+                }
+            }
         }
-      }
-    }
-  ]
+    ]
 };
