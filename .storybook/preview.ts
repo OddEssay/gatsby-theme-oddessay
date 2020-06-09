0ec9f34846be0d8c69b59679e448c17fc7944886
Paul Bennett-Freeman <clicknmix@gmail.com>
2020-06-09 15:43:55 +0100

    🔩 Hack to ensure gatsby Link works as expected in stories

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,37 +1,38 @@
 import { configure } from '@storybook/react';
-import '../src/layouts/index.css'
+import '../src/layouts/index.css';
 // automatically import all files ending in *.stories.tsx
-configure(require.context('../src', true, /\.stories\.tsx?$/), module)
+configure(require.context('../src', true, /\.stories\.tsx?$/), module);
 import { addDecorator } from '@storybook/react';
 import { withInfo } from '@storybook/addon-info';
 
 // Globally in your .storybook/config.js, or alternatively, per-chapter
 addDecorator(
-    withInfo({
-        styles: {
-            header: {
-                h1: {
-                    marginRight: '20px',
-                    fontSize: '25px',
-                    display: 'inline',
-                },
-                body: {
-                    paddingTop: 0,
-                    paddingBottom: 0,
-                },
-                h2: {
-                    display: 'inline',
-                    color: '#999',
-                },
-            },
-            infoBody: {
-                backgroundColor: '#eee',
-                padding: '0px 5px',
-                lineHeight: '2',
-            },
+  withInfo({
+    styles: {
+      header: {
+        h1: {
+          marginRight: '20px',
+          fontSize: '25px',
+          display: 'inline',
         },
-        inline: true,
-        source: true,
-    })
+        body: {
+          paddingTop: 0,
+          paddingBottom: 0,
+        },
+        h2: {
+          display: 'inline',
+          color: '#999',
+        },
+      },
+      infoBody: {
+        backgroundColor: '#eee',
+        padding: '0px 5px',
+        lineHeight: '2',
+      },
+    },
+    inline: true,
+    source: true,
+  })
 );
-global.__PATH_PREFIX__ = ""
+global.__PATH_PREFIX__ = '';
+global.__BASE_PATH__ = ''; // Used to make gatsby-link play nicely
