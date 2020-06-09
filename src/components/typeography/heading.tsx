bcae1620ece02c86e63e69b5488c4eae83290b91
Paul Bennett-Freeman <clicknmix@gmail.com>
2020-06-09 09:59:51 +0100

    🎨 Add some styling to base symantic HTML elements
      * Styling the base HTML elements makes using markdown easier since
        it can be used without needing to add additional custom element mappings
      * The 'code' element styling clashes with Storybook but can be overlooked

--- src/components/typeography/heading.tsx
+++ src/components/typeography/heading.tsx
@@ -1,17 +1,20 @@
-import * as React from "react";
-import './headings.css'
-import '../../layouts/index.css'
+import * as React from 'react';
+import './headings.css';
+import '../../layouts/index.css';
 
-export interface HeadingProps { title: String; }
+export interface HeadingProps {
+  title: String;
+}
 
-export const Heading = ({title}: HeadingProps) => <h1
-        className='heading
-            text-2xl
+export const Heading = ({ title }: HeadingProps) => (
+  <h1
+    className="heading
             tablet:text-blue-500
             laptop:text-yellow-500
             desktop:text-purple-500  desktop:text-5xl
             widescreen:text-red-500 widescreen:text-6xl
-        '
-    >{title}
-</h1>;
-
+        "
+  >
+    {title}
+  </h1>
+);
--- src/components/typeography/typography.stories.tsx
+++ src/components/typeography/typography.stories.tsx
@@ -1,21 +1,51 @@
-import * as React from "react";
+import * as React from 'react';
 
-import {Heading, Text, Title} from "./index";
+import { Heading, Text, Title } from './index';
 
 export default { title: 'Typography' };
 
-export const heading = () => <Heading title='This is a title' />
+export const heading = () => <Heading title="This is a title" />;
 
-export const text = () => <Text>This is some text</Text>
-export const long_text = () => <Text>
-    This is some text spread over
-    multiple lines.
-</Text>
+export const text = () => <Text>This is some text</Text>;
+export const long_text = () => (
+  <Text>This is some text spread over multiple lines.</Text>
+);
 
-export const page_example = () => <>
-    <Heading title='This is a top level heading' />
-    <Title title='This a content title' />
+export const page_example = () => (
+  <>
+    <Heading title="This is a top level heading" />
+    <Title title="This a content title" />
     <Text>This is some text</Text>
     <Text>This is some text</Text>
     <Text>This is some text</Text>
-    </>
+  </>
+);
+
+export const symantic_html = () => (
+  <>
+    <h1>Heading 1</h1>
+    <h2>Heading 2</h2>
+    <h3>Heading 3</h3>
+    <h4>Heading 4</h4>
+    <h5>Heading 5</h5>
+    <h6>Heading 6</h6>
+    <code>Code Block</code>
+    <pre>Pre Tag Second Line 3rd Line</pre>
+    <p>
+      This is a paragraph of text.
+      <br /> With a second line.
+    </p>
+    <p>
+      This is a paragraph of text. <br /> With a second line.
+    </p>
+    <p>
+      This is a paragraph of text. <br />
+      With a second line.
+    </p>
+    <ul>
+      <li>List 1</li>
+      <li>List 2</li>
+      <li>List 3</li>
+    </ul>
+  </>
+);
