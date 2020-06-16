967c80a3c074f835f61543cd710c1b8fc0a5d65a
Paul Bennett-Freeman <clicknmix@gmail.com>
2020-06-16 18:21:36 +0100

    🔩 Configure Storybook to strip graphql from components
      * Resolves issues with definging graphql fragments in component files

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -3,11 +3,17 @@ const path = require('path')
 module.exports = ({ config, mode }) => {
     config.module.rules.push({
         test: /\.(ts|tsx)$/,
-        use: [
-            {
+        use: [{
                 loader: require.resolve('babel-loader'),
                 options: {
-                    presets: [['react-app', { flow: false, typescript: true }]],
+                    presets: [
+                        ['react-app', { flow: false, typescript: true }]
+                    ],
+                    plugins: [
+                        require.resolve("@babel/plugin-proposal-class-properties"),
+                        // use babel-plugin-remove-graphql-queries to remove static queries from components when rendering in storybook
+                        require.resolve("babel-plugin-remove-graphql-queries"),
+                    ],
                 }
             },
             {
@@ -17,18 +23,16 @@ module.exports = ({ config, mode }) => {
     });
     config.module.rules.push({
         test: /\.css$/,
-        use: [
-            {
-                loader: "postcss-loader",
-                options: {
-                    ident: "postcss",
-                    plugins: [
-                        require("tailwindcss"),
-                        require("autoprefixer")
-                    ]
-                }
+        use: [{
+            loader: "postcss-loader",
+            options: {
+                ident: "postcss",
+                plugins: [
+                    require("tailwindcss"),
+                    require("autoprefixer")
+                ]
             }
-        ],
+        }],
         include: path.resolve(__dirname, "../")
     });
     config.resolve.extensions.push('.ts', '.tsx');
